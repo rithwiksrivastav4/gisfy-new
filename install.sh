c4fd3f35ebb2ae0c8924a33db2d0dcdaa9852bf0
rithwiksrivastav4 <srivastavrithwik@gmail.com>
2025-10-18 17:17:32 +0530

    added

--- install.sh
+++
@@ -1,6 +0,0 @@
-sudo apt update
-sudo apt install -y docker.io
-sudo systemctl enable docker
-sudo systemctl start docker
-sudo usermod -aG docker $USER
-docker run -d -p 8000:8000 --name django-app rithwik504/basic-project:latest
